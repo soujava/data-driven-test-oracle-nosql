9bf2c3e6158da2aa822eebbdb6183ae04ebb9d9d
Otavio Santana <otaviopolianasantana@gmail.com>
2025-07-28 08:51:43 +0100

    test: update room service
    Signed-off-by: Otavio Santana <otaviopolianasantana@gmail.com>

--- src/test/java/org/soujava/demos/mongodb/document/RoomRepositoryTest.java
+++ src/test/java/org/soujava/demos/mongodb/document/RoomRepositoryTest.java
@@ -39,8 +39,6 @@ class RoomRepositoryTest {
     @Inject
     private RoomRepository repository;
 
-    private static final  Faker FAKER = new Faker();
-
     @BeforeEach
     void setUP() {
 
@@ -180,23 +178,8 @@ class RoomRepositoryTest {
     }
 
     static Stream<Arguments> room() {
-        return Stream.of(Arguments.of(getRoom(), Arguments.of(getRoom(), Arguments.of(getRoom()))));
+        return Stream.of(Arguments.of(RoomFaker.getRoom(), Arguments.of(RoomFaker.getRoom(),
+                Arguments.of(RoomFaker.getRoom()))));
     }
 
-    private static Room getRoom() {
-        return Room.builder()
-                .id(UUID.randomUUID().toString())
-                .roomNumber(FAKER.number().numberBetween(100, 999))
-                .type(randomEnum(RoomType.class))
-                .status(randomEnum(RoomStatus.class))
-                .cleanStatus(randomEnum(CleanStatus.class))
-                .smokingAllowed(FAKER.bool().bool())
-                .build();
-    }
-
-    private static <T extends Enum<?>> T randomEnum(Class<T> enumClass) {
-        T[] constants = enumClass.getEnumConstants();
-        int index = ThreadLocalRandom.current().nextInt(constants.length);
-        return constants[index];
-    }
 }
--- src/test/java/org/soujava/demos/mongodb/document/RoomServiceTest.java
+++ src/test/java/org/soujava/demos/mongodb/document/RoomServiceTest.java
@@ -1,5 +1,6 @@
 package org.soujava.demos.mongodb.document;
 
+import jakarta.inject.Inject;
 import org.eclipse.jnosql.mapping.Database;
 import org.eclipse.jnosql.mapping.core.Converters;
 import org.eclipse.jnosql.mapping.document.DocumentTemplate;
@@ -10,6 +11,9 @@ import org.eclipse.jnosql.mapping.semistructured.EntityConverter;
 import org.jboss.weld.junit5.auto.AddExtensions;
 import org.jboss.weld.junit5.auto.AddPackages;
 import org.jboss.weld.junit5.auto.EnableAutoWeld;
+import org.junit.jupiter.api.Test;
+
+import java.util.UUID;
 
 @EnableAutoWeld
 @AddPackages(value = {Database.class, EntityConverter.class, DocumentTemplate.class})
@@ -19,4 +23,26 @@ import org.jboss.weld.junit5.auto.EnableAutoWeld;
 @AddPackages(Converters.class)
 @AddExtensions({ReflectionEntityMetadataExtension.class, DocumentExtension.class})
 class RoomServiceTest {
+
+    @Inject
+    private RoomService service;
+
+
+    @Test
+    void shouldSaveRoom() {
+        service.newRoom(room);
+    }
+
+
+
+    private static Room getRoom() {
+        return Room.builder()
+                .id(UUID.randomUUID().toString())
+                .roomNumber(FAKER.number().numberBetween(100, 999))
+                .type(randomEnum(RoomType.class))
+                .status(randomEnum(RoomStatus.class))
+                .cleanStatus(randomEnum(CleanStatus.class))
+                .smokingAllowed(FAKER.bool().bool())
+                .build();
+    }
 }
